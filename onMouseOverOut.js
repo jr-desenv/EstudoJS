34a5a0f0dad1733bb501682292cb926b7da9c32a
jr-desenv <junior101994>
2019-05-31 04:14:58 -0300

    DOM

---
+++ onMouseOverOut.js
@@ -0,0 +1,7 @@
+function mouseOver(){
+    document.getElementById("mouseAlt").innerHTML = "Remove  Mouse";
+}
+
+function mouseOut(){
+    document.getElementById("mouseAlt").innerHTML = "Lean mouse";
+}
